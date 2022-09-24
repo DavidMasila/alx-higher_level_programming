66fd071b9ac33107019b0505bcdaa649deb187ca
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-09-23 18:02:37 -0700

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
+"""Check status"""
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    q = argv[1] if len(argv) == 2 else ""
-    url = 'http://0.0.0.0:5000/search_user'
-    r = requests.post(url, data={'q': q})
+
+def searchapi():
+    """status"""
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
     try:
-        r_dict = r.json()
-        id, name = r_dict.get('id'), r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+        data = result.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+            print("No result")
     except:
         print("Not a valid JSON")
+
+if __name__ == "__main__":
+    searchapi()
