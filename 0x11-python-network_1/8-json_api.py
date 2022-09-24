a0021854489231da76947af7d9731c62b607f44c
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-09-23 18:14:53 -0700

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
+"""
+Use requests package to make a post request to given URL with argument
+set in variable `q`, defaulting to empty string. If response body is properly
+JSON formatted and not empty, display `id` and `name` as given format.
+Otherwise display error message.
+"""
 import sys
-
-
-def searchapi():
-    """status"""
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
-
-    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-
-    try:
-        data = result.json()
-        if data:
-            print("[{}] {}".format(data["id"], data["name"]))
-        else:
-            print("No result")
-    except:
-        print("Not a valid JSON")
+import requests
 
 if __name__ == "__main__":
-    searchapi()
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        arg = ""
+    payload = {'q': arg}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
+    try:
+        r.raise_for_status()
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
