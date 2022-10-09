8b2b498932d2c2e7afcbe710319b188c322f5d3e
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 06:48:07 -0700

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,5 @@ import sys
 import requests 
 
 if __name__ == "__main__":
-    url=sys.argv[1]
-    email=sys.argv[2]
-    email_data={"email":email}
-
-    r = requests.post(url, data=email_data)
+    r = requests.post(sys.argv[1], data={"email":sys.argv[2]})
     print(r.text)
