e88a1f8049877726134d1b51d8d902d636a7189d
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 06:52:47 -0700

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
-"""
-    Script that takes in a URL and an email sends a POST request to that URL
-    with the email as a parameter.
-    Displays the body of the response
-"""
-import sys
-import requests
+#!/usr/bin/python3 
 
+import sys
+import requests 
 
 if __name__ == "__main__":
     r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
