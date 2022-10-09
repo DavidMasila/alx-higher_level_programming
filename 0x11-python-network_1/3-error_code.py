2b67dae36315f9896fbe28e2d2de661c67857812
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 05:55:52 -0700

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,19 @@
 #!/bin/bash/python3 
+
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
+from urllib.error import HTTPError
 import sys 
 
 
 if __name__=="__main__":
-    url = sys.argv[1]
-    email=urlencode({"email":sys.argv[2]}).encode("ascii")
+    url=sys.argv[1]
 
-    req = Request(url, email)
+    req = Request(url)
+
     with urlopen(req) as resp:
-        body=resp.read()
-        print(body.decode("utf-8"))
+        try:
+            body = resp.read()
+            print(body.decode("utf-8"))
+        except HTTPError as e :
+            print("Error code: ",e.code)
