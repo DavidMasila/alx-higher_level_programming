71e5f4643d8dea3f9e9630e6ddb7348521ff98d1
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 06:34:36 -0700

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+"""
+    Script that fetches https://intranet.htbtn.io/status
+"""
 
-import requests 
+import requests
+
 
 if __name__ == "__main__":
-    req = requests.get('https://alx-intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t-: {}".format(type(req.text)))
-    print("\t-:{}".format(req.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
