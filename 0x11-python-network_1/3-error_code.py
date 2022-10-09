8505eba5f08ee91a0468b5d3fcec9bbc48dc9e40
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 06:13:58 -0700

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,21 +1,19 @@
-#!/usr/bin/python3
-"""
-    A script that takes in a URL and sends a request to the URL
-    and displays the body of the response decoded in utf-8.
-    urllib.error.HTTPError is managed and printed in the form
-    Error code: <HTTP status code>
-"""
+#!/usr/bash/python3 
 
-import sys
-import urllib.request
-import urllib.parse
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+from urllib.error import HTTPError
+import sys 
 
 
-if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+if __name__=="__main__":
+    url=sys.argv[1]
+
+    req = Request(url)
+
     try:
-        with urllib.request.urlopen(req) as resp:
-            html = resp.read()
-            print("{}".format(html.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with urlopen(req) as resp:
+            body=resp.read()
+            print(body.decode("utf-8"))
+    except HTTPError as e:
+        print("Error code: ",e.code)
