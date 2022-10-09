e41cd67b566cda902249752985a948653804bba9
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-09 04:30:25 -0700

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-from urllib import request
+import urllib.request
 
-if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://alx-intranet.hbtn.io/status' 
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as resp:
+    some_data = resp.read()
+print(some_data)
