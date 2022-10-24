8cf66334826cbb983f132795fe1459d08041986f
MasilaDavid <davidmasila97@gmail.com>
2022-10-24 13:27:24 +0300

    API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// Write a script that prints the title of a Star Wars movie where the episode number matches a given integer.
-const process = require('process');
-const request = require('request');
+// Write a script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+const req = require('request');
+const args = require('process').argv;
 
-const endpoint = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-
-request.get(endpoint, (error, body) => {
-  if (error) {
-    console.log(error);
+req.get(`https://swapi-api.hbtn.io/api/films/${args[2]}`, (err, res, body) => {
+  if (err) {
+    console.log(err);
   } else {
     console.log(JSON.parse(body).title);
   }
