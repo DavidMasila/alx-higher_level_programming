5970d20ed9432a7449d73643341f89106c817417
Scorprog <99339505+DavidMasila@users.noreply.github.com>
2022-10-24 00:58:26 -0700

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,10 @@ const fs = require('fs')
 const process = require('process')
 
 fs.readFile(process.argv[1], 'utf-8', (err, data) =>{
-    if (err) throw err;
-    else{
-        console.log(data);
+    if (err){
+        console.log(err);
     }
-        
+    else{
+        console.log(data.toString());
+    }    
 })
