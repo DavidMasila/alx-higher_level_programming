dc06deb35f63b9afa23b10a5631249445162216b
MasilaDavid <davidmasila97@gmail.com>
2022-10-24 11:30:57 +0300

    Writing to file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const process = require('process');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-    console.log(err || data)
+  console.log(err || data);
 });
