be12cb8f590250a4af8c80423a221dfafc244bbc
MasilaDavid <davidmasila97@gmail.com>
2022-10-24 14:43:38 +0300

    API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,20 @@
 
 const request = require('request');
 const args = require('process').argv;
-
-const charID = 18;
-
 request(args[2], (error, res, body) => {
   if (error) {
     console.log(error);
   } else {
-    let i =0;
     let count = 0;
-    const moviesData = JSON.parse(body);
-    for (i=0; i <= moviesData.length; i++) {
-      if (moviesData['results']['episode_id'] === charID) {
-        count += 1;
+    const moviesData = JSON.parse(body).results;
+    for (let i = 0; i <= moviesData.length; i++) {
+      const characters = moviesData[i].characters;
+      for (let j = 0; j <= characters.length; j++) {
+        const char18 = characters[j].split('/');
+        if (char18[char18.length - 2] === '18') {
+          count++;
+          break;
+        }
       }
     }
     console.log(count);
