5cde9e51ff269642917818604eba1bbaf9d8b2cb
MasilaDavid <davidmasila97@gmail.com>
2022-10-25 05:51:05 -0700

    JS API

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Write a script that prints all characters of a Star Wars movie:
+
+const request = require('request');
+const args = require('process').argv;
+const url = `https://swapi-api.hbtn.io/api/films/${args[2]}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      request.get(characters[i], (error, response, body) => {
+        error ? console.log(error) : console.log(JSON.parse(body).name);
+      });
+    }
+  }
+});
