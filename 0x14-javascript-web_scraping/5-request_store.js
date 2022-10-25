b2375922fce221fa4377f30a7820d69511204dc7
MasilaDavid <davidmasila97@gmail.com>
2022-10-25 00:09:40 -0700

    js API

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,14 +3,4 @@
 const args = require('process').argv;
 const fs = require('fs');
 const request = require('request');
-
-request(args[2], (body, error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = body;
-    fs.writeFile(args[2], content, 'utf-8', (err) => {
-      if (err) throw err;
-    });
-  }
-});
+request(args[2]).pipe(fs.createWriteStream(args[3], 'utf-8'));
